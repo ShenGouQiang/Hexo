36285f6e2d47069d7928690735f78915c40bccda
沈构强 <shengouqiang@Shen-LocalPC.lan>
2020-07-21 00:48:43 +0800

    fix create shell

--- createNewPage.sh
+++ createNewPage.sh
@@ -1,33 +1,82 @@
 #!/bin/bash
-# 使用举例：./createNewPage.sh aa testOne "其他,学习,lean,other" "test,学习,其他,other"
+# 使用举例：./createNewPage.sh -d aa -f testOne -c "其他,学习,lean,other" -t "test,学习,其他,other"
 readonly path="source/_posts"
 realPath="$path"
 cd $path
 #第一个参数是page所在的目录的名字，如果为''，则认为没有目录，直接在根目录创建
 #如果是根目录，不用做任何操作
 #如果为根目录，判断目录是否存在，如果不存在，则直接创建目录
+
+
 mulu=$1
-if [ "$mulu" = "" ];then
+fileName=$2
+categoriesStr=$3
+tagStr=$4
+
+while getopts ":d:f:c:t:" OPTNAME
+do
+  case "${OPTNAME}" in
+    "d")
+      mulu=${OPTARG}
+      ;;
+    "f")
+      fileName=${OPTARG}
+      ;;
+    "c")
+      categoriesStr=${OPTARG}
+      ;;
+    "t")
+      tagStr=${OPTARG}
+      ;;
+    ":")
+      exit 1;
+      ;;
+  esac
+done
+
+if test -z "$mulu"
+then
 	echo "filePath is null"
 else
 	echo "filePath is $mulu"
-	if [ ! -d "$mulu" ];then
-		mkdir -p $mulu
-		echo "filePath $mulu is created success"
-	else
-		echo "filePath $mulu has been exist"
-	fi
-	realPath="$mulu"
 fi
+
+if test -z "$fileName"
+then
+        echo "fileName is null"
+else
+        echo "fileName is $fileName"
+fi
+
+if test -z "$categoriesStr"
+then
+        echo "Categories is null"
+else
+        echo "Categories is $categoriesStr"
+fi
+
+if test -z "$tagStr"
+then
+        echo "tag is null"
+else
+        echo "tag is $tagStr"
+fi
+
+if [ ! -d "$mulu" ];then
+	mkdir -p $mulu
+	echo "filePath $mulu is created success"
+else
+	echo "filePath $mulu has been exist"
+fi
+
+realPath="$mulu"
 #第二个参数是page的名字
-fileName=$2
 hexo new "$fileName"
 if [ "$path" != "$realPath" ];then
 	mv "$fileName.md" $realPath
 	cd $realPath
 fi
 #第三个参数是categories，也就是目录,如果有多个，用","进行分割
-categoriesStr=$3
 categoriesArray=(${categoriesStr//,/ })  
 i=0
 leni=${#categoriesArray[*]}
@@ -45,7 +94,6 @@ do
 	" "$fileName.md"
 done
 #第四个参数时tag，也就是标签，如果有多个，用","进行分割
-tagStr=$4
 tagArray=(${tagStr//,/ })  
 j=0
 lenj=${#tagArray[*]}
